69dee1e85e7013a0a6e1b2a2537f54758f341475
kwin555 <ktn555@att.net>
2018-03-26 10:50:31 -0500

    PlusMinus

--- PlusMinus/src/com/company/Main.java
+++ PlusMinus/src/com/company/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int [] array = {-4, 3, -9, 0, 4, 1};
+        plusMinus(array);
+    }
+    public static void plusMinus(int[] array) {
+        double postiveCount = 0;
+        double negativeCount = 0;
+        double zeroCount = 0;
+
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > 0) {
+                postiveCount++;
+            } else if (array[i] == 0) {
+                zeroCount++;
+            } else {
+                negativeCount++;
+            }
+        }
+        double totalNumber = array.length;
+        System.out.println(postiveCount / totalNumber);
+        System.out.println(negativeCount / totalNumber);
+        System.out.println(zeroCount/ totalNumber);
     }
 }
